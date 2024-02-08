d8c452fa8de2d4355df163e9f0de7729a8a4dd6d
maccan-tech <mmarcus.beckman@gmail.com>
2024-02-08 15:54:28 +0100

    small fixes

--- roles/nvim/files/lua/maccan/plugins/colorscheme.lua
+++ roles/nvim/files/lua/maccan/plugins/colorscheme.lua
@@ -43,7 +43,12 @@ return {
     name = "rose-pine",
     priority = 1002,
     config = function()
-      vim.cmd([[colorscheme rose-pine]])
+      require('rose-pine').setup({
+        -- disable_background = true,
+      })
+
+      vim.cmd("colorscheme rose-pine")
+
     end,
   },
 }
