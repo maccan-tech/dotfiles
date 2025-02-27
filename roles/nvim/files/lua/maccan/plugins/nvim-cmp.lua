16b54f7bb00e505d1ac0e7ecfd5fbb02f72c0ccd
maccan-tech <mmarcus.beckman@gmail.com>
2025-02-27 09:12:18 +0100

    Change mapping for nvim-cmp.

--- roles/nvim/files/lua/maccan/plugins/nvim-cmp.lua
+++ roles/nvim/files/lua/maccan/plugins/nvim-cmp.lua
@@ -30,13 +30,21 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        -- ["<C-Up>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        -- ["<C-Down>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-Up>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-Down>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ['<Down>'] = cmp.mapping(function(fallback)
+          cmp.close()
+        	fallback()
+        	end, { "i" }),
+        ['<Up>'] = cmp.mapping(function(fallback)
+        	cmp.close()
+        	fallback()
+          	end, { "i" }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
