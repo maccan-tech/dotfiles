bfbd08d1153ae4e75f4d3bc6b9a221dec0148a9d
maccan-tech <mmarcus.beckman@gmail.com>
2025-03-13 12:14:33 +0100

    Modyfi screenshot script

--- roles/hypr/files/hypr/scripts/screenshot.sh
+++ roles/hypr/files/hypr/scripts/screenshot.sh
@@ -7,25 +7,23 @@ swpy_dir="$HOME/.config/swappy"
 mkdir -p $swpy_dir
 echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
 
-
 option2="Selected area"
 option3="Fullscreen (delay 1 sec)"
 
 options="$option2\n$option3"
 
-choice=$(echo -e "$options" | rofi -dmenu -i -no-show-icons -l 4 -width 30 -p "Take Screenshot")
+choice=$(echo -e "$options" | rofi -dmenu -i -no-show-icons -config ~/.config/rofi/config-singlecol.rasi -l 4 -width 30 -p "Take Screenshot")
 
 case $choice in
     $option2)
       grim -g "$(slurp)" - | swappy -f - 
-      # dunstify $ncolor "theme" -a "saved in $save_dir" -i "$save_dir/$save_file" -r 91190 -t 4200 --urgency="normal"
-      # dunstify $ncolor "theme" -a "saved in $save_dir" -r 91190 -t 4200 --icon="~/.config/dunst/icons/screenshot.svg" --urgency="normal"
-      dunstify "Screenshot" "saved in $save_dir" -i ~/.config/dunst/icons/screenshot.svg -u "normal"
+      notify-send "Screenshot" "saved in $save_dir" -u "normal"
     ;;
     $option3)
       sleep 1
       grim - | swappy -f -
-      # dunstify $ncolor "theme" -a "saved in $save_dir" -r 91190 -t 4200 --icon="~/.config/dunst/icons/screenshot.svg" --urgency="normal"
-      dunstify "Screenshot" "saved in $save_dir" -i ~/.config/dunst/icons/screenshot.svg -u "normal"
+      notify-send "Screenshot" "saved in $save_dir" -u "normal"
     ;;
 esac
+u-l 4 -width 30 -p "Take Screenshot")
+
