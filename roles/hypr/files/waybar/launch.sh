8e13986a4df1b3f21b770414511389d8820f3b20
maccan-tech <mmarcus.beckman@gmail.com>
2025-03-18 12:21:54 +0100

    Small fixes to waybar theme.

--- roles/hypr/files/waybar/launch.sh
+++ roles/hypr/files/waybar/launch.sh
@@ -8,12 +8,4 @@
 # ----------------------------------------------------- 
 killall waybar
 
-# ----------------------------------------------------- 
-# Loading the configuration based on the username
-# ----------------------------------------------------- 
-if [[ $USER = "raabe" ]]
-then
-    waybar -c ~/.config/waybar/myconfig & -s ~/.config/waybar/style.css  
-else
-    LC_TIME="sv_SE.utf8" waybar &
-fi 
+LC_TIME="sv_SE.utf8" waybar &
