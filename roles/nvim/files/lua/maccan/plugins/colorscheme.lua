e9b95a4c2d3275bdcaac846e6cfbc514e6bc48f6
maccan-tech <mmarcus.beckman@gmail.com>
2025-03-21 09:21:05 +0100

    Add nvim colorschemes.

--- roles/nvim/files/lua/maccan/plugins/colorscheme.lua
+++ roles/nvim/files/lua/maccan/plugins/colorscheme.lua
@@ -13,30 +13,33 @@ return {
 --      vim.cmd([[colorscheme nord]])
 --    end,
 --  },
---  {
---    -- https://github.com/catppuccin/nvim
---    "catppuccin/nvim",
---    name = "catppuccin",
---    priority = 1001,
---    config = function()
---      local catppaccin = require("catppuccin")
---
---      catppaccin.setup({
---        flavour = "frappe", -- latte, frappe, macchiato, mocha
---        integrations = {
---          headlines = true,
---          cmp = true,
---          nvimtree = true,
---          treesitter = true,
---          aerial = true,
---          native_lsp = {
---            enabled = true,
---          },
---        }
---      })
---      vim.cmd([[colorscheme catppuccin]])
---    end,
---  },
+  { "rebelot/kanagawa.nvim",    lazy = false },
+  { "sho-87/kanagawa-paper.nvim"    , lazy = false, priority = 1000, opts = {}, },
+  { "sainnhe/gruvbox-material", lazy = false, event = "VeryLazy", version = "*" },
+  {
+    -- https://github.com/catppuccin/nvim
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1001,
+    config = function()
+     local catppaccin = require("catppuccin")
+
+     catppaccin.setup({
+       flavour = "frappe", -- latte, frappe, macchiato, mocha
+       transparent_background = true,
+       integrations = {
+         headlines = true,
+          cmp = true,
+          nvimtree = true,
+          treesitter = true,
+          aerial = true,
+          native_lsp = {
+            enabled = true,
+          },
+        }
+      })
+    end,
+  },
   {
     -- https://github.com/rose-pine/neovim?tab=readme-ov-file
     "rose-pine/neovim",
@@ -46,9 +49,6 @@ return {
       require('rose-pine').setup({
         disable_background = true,
       })
-
-      vim.cmd("colorscheme rose-pine")
-
     end,
   },
 }
