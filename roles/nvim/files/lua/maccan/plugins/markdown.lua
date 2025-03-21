4617da11f91dffdf7badb1fb148f43ed9942a5c9
maccan-tech <mmarcus.beckman@gmail.com>
2025-03-21 08:51:06 +0100

    Change nvim markdown plugin.

---
+++ roles/nvim/files/lua/maccan/plugins/markdown.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "tadmccorkle/markdown.nvim",
+    ft = "markdown", -- or 'event = "VeryLazy"'
+    opts = {
+    },
+  },
+  { 
+    --nice markdown inline rendering
+    'MeanderingProgrammer/render-markdown.nvim',
+    event = "VeryLazy",
+    priority = 1000,
+    ft = { "markdown" },
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    keys = {
+      { "<leader>mr", ":RenderMarkdown toggle<CR>", desc = "Markdown Render Toggle" }
+    },
+    config = function()
+      require("render-markdown").setup({
+        heading = {
+          sign = false,
+          position = "inline",
+          icons = { '# ', '## ', '### ', '#### ', '##### ', '###### ' },
+          width = 'block',
+          left_pad = 2,
+          right_pad = 4,
+        },
+        code = {
+          sign = false,
+          left_pad = 2,
+          right_pad = 4,
+          border = "thick",
+        },
+        bullet = { right_pad = 2 },
+      })
+    end,
+  },
+}
