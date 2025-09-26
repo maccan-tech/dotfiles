f860f781937d82aaf0095379209025008444781f
maccan-tech <mmarcus.beckman@gmail.com>
2025-09-26 14:22:25 +0200

    Add file restore manu with rofi
    Signed-off-by: maccan-tech <mmarcus.beckman@gmail.com>

---
+++ roles/hypr/files/rofi/trash/trash-restore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Visa listan i rofi och spara det användaren väljer
+chosen_file=$(trash-list | cut -d' ' -f3- | rofi -dmenu -config ~/.config/rofi/trash/style.rasi)
+
+# Om användaren valde något (inte avbröt med Esc t.ex.)
+if [[ -n "$chosen_file" ]]; then
+    printf "0\n" | trash-restore "$chosen_file"
+    notify-send "Restored" "$chosen_file"
+fi
+
