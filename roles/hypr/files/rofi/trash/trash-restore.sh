14e6770bb1123ba01553f468a363e0f34b78b543
maccan-tech <mmarcus.beckman@gmail.com>
2025-09-26 14:43:29 +0200

    Fix style on rofi restore menu
    Signed-off-by: maccan-tech <mmarcus.beckman@gmail.com>

--- roles/hypr/files/rofi/trash/trash-restore.sh
+++ roles/hypr/files/rofi/trash/trash-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Visa listan i rofi och spara det användaren väljer
-chosen_file=$(trash-list | cut -d' ' -f3- | rofi -dmenu -config ~/.config/rofi/trash/style.rasi)
+chosen_file=$(trash-list | cut -d' ' -f3- | rofi -dmenu -mesg "Restore File" -config ~/.config/rofi/trash/style.rasi)
 
 # Om användaren valde något (inte avbröt med Esc t.ex.)
 if [[ -n "$chosen_file" ]]; then
