f36d25b48306dbd74bf2450e291f9863c935e25f
maccan-tech <mmarcus.beckman@gmail.com>
2026-02-19 13:12:18 +0100

    Change supermaven-nvim accept keybindins
    Signed-off-by: maccan-tech <mmarcus.beckman@gmail.com>

--- roles/nvim/files/lua/maccan/plugins/supermaven.lua
+++ roles/nvim/files/lua/maccan/plugins/supermaven.lua
@@ -1,18 +1,24 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  event = "InsertEnter",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_suggestion = "<Tab>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
-      },
-      ignore_filetypes = { "gitcommit", "TelescopePrompt" },
-      color = {
-        suggestion_color = "#808080",
-      },
-      log_level = "off",
-    })
-  end,
+	"supermaven-inc/supermaven-nvim",
+	event = "InsertEnter",
+	config = function()
+		local suggestion = require("supermaven-nvim.completion_preview")
+
+		require("supermaven-nvim").setup({
+			keymaps = {
+				accept_suggestion = "<C-l>",
+				clear_suggestion = "<C-]>",
+				accept_word = "<C-j>",
+			},
+			ignore_filetypes = { "gitcommit", "TelescopePrompt" },
+			color = {
+				suggestion_color = "#808080",
+			},
+			log_level = "off",
+		})
+
+		vim.keymap.set("i", "<C-Right>", function()
+			suggestion.on_accept_suggestion()
+		end, { desc = "Accept Supermaven suggestion" })
+	end,
 }
