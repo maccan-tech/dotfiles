86578aac2cbe42d8261ed9ed3747fe18e18077ed
maccan-tech <mmarcus.beckman@gmail.com>
2026-02-23 13:13:17 +0100

    Remove yaml formatter from conform to prevent unwanted reformatting

--- roles/nvim/files/lua/maccan/plugins/conform.lua
+++ roles/nvim/files/lua/maccan/plugins/conform.lua
@@ -14,7 +14,6 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        yaml = { "prettier" },
         lua = { "stylua" },
       },
       format_on_save = {
